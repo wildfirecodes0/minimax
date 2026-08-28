77b0a09ac415040ed4d930c6982a0ab6fcc8f958
Raushan <raushankakhaura0@gmail.com>
2026-08-28 10:01:13 +0530

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   BOT_NAME: 'Mini Max Seller Bot',
-  BOT_USERNAME: 'MiniMaxSellerBot',
+  BOT_USERNAME: 'MiniMaxStoreBot',
   FORCE_JOIN_CHANNEL_USERNAME: '@MiniMaxStoreShop',
   FORCE_JOIN_CHANNEL_LINK: 'https://t.me/MiniMaxStoreShop',
   FORCE_JOIN_CHANNEL_NAME: 'Mini Max Store',
